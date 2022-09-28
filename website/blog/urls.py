c6d0b12d774e81f07fd936d403ad7019dd7d05f7
unknown <m.bolhassani@gmail.com>
2022-09-28 16:39:48 +0330

    create  update and delete views also create their templetes

--- website/blog/urls.py
+++ website/blog/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('',views.last_posts,name='last-posts'),
     path('all-posts/', views.all_posts, name='all-post'),
     path('single-post/<slug:slug>', views.single_post, name='single-post'),
+    path('create-post', views.create_post, name='create-post'),
+    path('update-post/<slug:slug>', views.update_post, name='update-post'),
+    path('delete-post/<slug:slug>', views.delete_post, name='delete-post'),
 
 ]
--- website/blog/views.py
+++ website/blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.shortcuts import render, get_object_or_404
+
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
 
@@ -7,37 +7,76 @@ from .forms import PostForm, CommentForm
 
 
 def last_posts(request):
-    last_post=Post.objects.all().order_by('-date')[:3]
+    last_post = Post.objects.all().order_by('-date')[:3]
 
-    context={
+    context = {
         'last_post': last_post
     }
-    return render(request, 'blog/last-posts.html',context)
+    return render(request, 'blog/last-posts.html', context)
 
 
 def all_posts(request):
-    posts=Post.objects.all()
- 
-    context={'posts':posts}
+    posts = Post.objects.all()
+
+    context = {'posts': posts}
     return render(request, 'blog/all-posts.html', context)
 
 
 def single_post(request, slug):
-    post=Post.objects.get(slug=slug)
+
+    # post=Post.objects.get(slug=slug)
+    post = get_object_or_404(Post, slug=slug)
     tags = post.tags.all()
     comments = post.comments.all()
     comment_form = CommentForm()
-    context={
-        'post':post,
-        'tags':tags,
+    context = {
+        'post': post,
+        'tags': tags,
         'comments': comments,
         'comment_form': comment_form,
     }
-    if request.method=='POST':
+    if request.method == 'POST':
         comment_form = CommentForm(request.POST)
         if comment_form.is_valid():
-           comment= comment_form.save(commit=False)
-           comment.post=post
-           comment.save()
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
 
-    return render(request, 'blog/single-post.html',context)
+    return render(request, 'blog/single-post.html', context)
+
+
+def create_post(request):
+    if request.method == "POST":
+        post_form = PostForm(request.POST)
+        if post_form.is_valid():
+            post_form.save()
+
+    else:
+        post_form = PostForm()
+
+    context = {
+        'post_form': post_form
+    }
+    return render(request, 'blog/create-post.html', context)
+
+
+def delete_post(request, slug):
+    if request.method == "POST":
+        post = get_object_or_404(Post, slug=slug)
+        post.delete()
+        return redirect('all-post')
+    return render(request, 'blog/delete-post.html')
+
+
+def update_post(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    post_form = PostForm(instance=post)
+    if request.method == "POST":
+        post_form = PostForm(request.POST, instance=post)
+        if post_form.is_valid():
+            post_form.save()
+            return redirect('all-post')
+
+    return render(request, 'blog/update-post.html',
+                  {'post_form': post_form}
+                  )
